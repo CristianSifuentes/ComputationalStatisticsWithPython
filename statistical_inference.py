f523ebe27cd2d73ef333067ff62ae84915d24350
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-09-12 13:52:03 -0600

    Creating examples using statical inference and statics

---
+++ statistical_inference.py
@@ -0,0 +1,21 @@
+'''
+Sí, estoy familiarizado con el concepto de inferencia estadística. La inferencia estadística es una rama de la estadística que se centra en la toma de decisiones y conclusiones sobre una población basándose en información o datos recopilados de una muestra representativa de esa población. En otras palabras, se refiere al proceso de hacer generalizaciones, estimaciones, predicciones y pruebas de hipótesis sobre características de una población utilizando información limitada de una muestra de esa población.
+
+La inferencia estadística se divide en dos categorías principales:
+
+1. **Estimación Estadística:** En esta parte de la inferencia estadística, se busca estimar o predecir características o parámetros de una población a partir de datos muestrales. Algunos de los conceptos clave incluyen:
+
+   - **Estimación de parámetros:** Se utilizan estadísticas muestrales, como la media y la varianza, para estimar parámetros poblacionales, como la media y la varianza poblacional.
+   
+   - **Intervalos de Confianza:** Se construyen intervalos de confianza alrededor de las estimaciones para proporcionar un rango dentro del cual se espera que esté el valor verdadero del parámetro poblacional con cierto nivel de confianza.
+
+2. **Pruebas de Hipótesis:** En esta parte de la inferencia estadística, se evalúan afirmaciones o hipótesis sobre una población en función de datos muestrales. Algunos conceptos clave incluyen:
+
+   - **Hipótesis Nula (H0) y Alternativa (H1):** Se plantean dos hipótesis: la hipótesis nula (H0), que representa una afirmación general o una situación de no cambio, y la hipótesis alternativa (H1), que representa una afirmación específica o una situación de cambio.
+   
+   - **Prueba de Hipótesis:** Se realiza una prueba estadística para determinar si los datos proporcionan evidencia suficiente para rechazar la hipótesis nula en favor de la hipótesis alternativa.
+   
+   - **Nivel de Significación:** Se elige un nivel de significación (alfa, α) que representa la probabilidad máxima de cometer un error de Tipo I (rechazar incorrectamente H0).
+
+La inferencia estadística se aplica en una amplia variedad de campos, como la investigación científica, la toma de decisiones empresariales, la medicina, la economía, la ingeniería y más. Es una herramienta esencial para sacar conclusiones basadas en datos y tomar decisiones informadas cuando no es posible analizar toda la población de interés.
+'''
