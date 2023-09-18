774dec9a9e12933d42befd49c3c32ab90f8cd76c
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-09-18 15:36:47 -0600

    Creating example for differentiate deterministic and stochastic

---
+++ deterministic_and_stochastic.py
@@ -0,0 +1,22 @@
+'''
+Por supuesto, la programación determinística y estocástica son dos enfoques diferentes en el campo de la programación y la resolución de problemas. Aquí está la diferencia clave entre ambas:
+
+1. **Programación Determinística:**
+
+   - **Naturaleza Determinista:** En la programación determinística, todos los eventos y resultados se basan en reglas fijas y predecibles. Esto significa que, dado un conjunto de condiciones iniciales específicas, el programa siempre producirá el mismo resultado.
+   
+   - **Ausencia de Incertidumbre:** En este enfoque, no hay incertidumbre o aleatoriedad en los datos o el proceso. Los valores de entrada, las operaciones y los resultados son precisos y conocidos de antemano.
+   
+   - **Ejemplos:** La mayoría de los programas de software convencionales, como las calculadoras, los procesadores de texto y las bases de datos, se basan en la programación determinística. También es común en la programación de sistemas y aplicaciones empresariales.
+
+2. **Programación Estocástica:**
+
+   - **Naturaleza Estocástica (Aleatoria):** En la programación estocástica, los resultados están sujetos a la aleatoriedad o la incertidumbre. Los eventos y resultados no son siempre predecibles y pueden variar incluso con las mismas condiciones iniciales.
+   
+   - **Presencia de Incertidumbre:** En este enfoque, se incorpora la incertidumbre a través de variables aleatorias o procesos estocásticos. Las probabilidades y distribuciones de probabilidad juegan un papel central en la toma de decisiones y la modelización.
+   
+   - **Ejemplos:** La programación estocástica se utiliza en aplicaciones y sistemas donde la aleatoriedad es inherente, como en simulaciones, modelos de riesgo financiero, juegos de azar, procesos de toma de decisiones bajo incertidumbre y muchos otros casos donde los resultados no son deterministas.
+
+En resumen, la principal diferencia entre programación determinística y estocástica radica en la naturaleza de los datos y los resultados. La programación determinística se basa en reglas fijas y produce resultados predecibles y repetibles, mientras que la programación estocástica incorpora incertidumbre y aleatoriedad, lo que la hace adecuada para modelar situaciones en las que los resultados pueden variar debido a factores aleatorios o desconocidos. Ambos enfoques son valiosos y se aplican en diferentes dominios según las necesidades del problema que se esté abordando.
+
+'''
