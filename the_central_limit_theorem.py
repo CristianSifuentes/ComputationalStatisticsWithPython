b734c906be24fb208ac804747d646ca6d4226cf6
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-10-03 16:42:50 -0600

    Add information

--- the_central_limit_theorem.py
+++ the_central_limit_theorem.py
@@ -1,3 +1,7 @@
+'''
+Claro, aquí tienes un programa en Python avanzado que implementa el Teorema del Límite Central de manera avanzada. Este programa generará una distribución de muestras aleatorias a partir de una distribución exponencial y demostrará cómo, a medida que aumenta el tamaño de la muestra, la distribución de las medias de esas muestras se aproxima a una distribución normal.
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import expon, norm
@@ -34,3 +38,17 @@ plt.ylabel('Densidad de Probabilidad')
 plt.legend()
 plt.grid(True)
 plt.show()
+
+'''
+Este programa realiza lo siguiente:
+
+Genera numero_muestras muestras de tamaño tamano_muestra a partir de una distribución exponencial con el parámetro de tasa lmbda.
+
+Calcula las medias de cada una de las muestras.
+
+Crea un histograma de las medias de las muestras y superpone una distribución normal teórica con la misma media y desviación estándar calculadas.
+
+Muestra un gráfico que demuestra cómo la distribución de las medias de las muestras se aproxima a una distribución normal a medida que numero_muestras aumenta.
+
+Este programa ilustra el Teorema del Límite Central al demostrar cómo las medias de las muestras se comportan de manera similar a una distribución normal, independientemente de la forma de la distribución original, cuando el tamaño de la muestra es lo suficientemente grande.
+'''
